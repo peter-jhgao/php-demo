87a7e59b6b9eac094fa6e33dfb66db2dd05ce02c
peter-jhgao <29678838@qq.com>
2018-04-21 15:16:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,15 @@
   <title>PHP 测试</title>
  </head>
  <body>
- <?php echo '<h2>Hello CCE 2</h2>'; ?>
+ <?php echo '<h2>Hello PHP</h2>'; ?>
+ <?php
+  $ch = curl_init();
+  $str ='http://peter-spring-mesher-demo';
+  curl_setopt($ch, CURLOPT_URL, $str);
+  curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+  $output = curl_exec($ch);
+  var_dump( $output );
+ ?>
  <?php phpinfo(); ?>
  </body>
 </html>
